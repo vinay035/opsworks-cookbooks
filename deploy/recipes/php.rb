23739dfb1004763d4544e3854423be117a77a16f
ravindragit <maurya.ravindra84@gmail.com>
2013-03-02 19:03:44 +0530

    Update php.rb

--- deploy/recipes/php.rb
+++ deploy/recipes/php.rb
@@ -5,7 +5,7 @@
 
 include_recipe "mod_php5_apache2"
 include_recipe "mod_php5_apache2::php"
-File.chmod(777,deploy[:deploy_to])
+File.chmod(0777,deploy[:deploy_to])
 print "permission changed\n"
 print deploy[:deploy_to]
 
